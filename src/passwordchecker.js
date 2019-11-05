20c50c9e72209409d5d12eb73529242550a67133
Sekgomotso_s <motsoshalang@gmail.com>
2019-11-05 16:28:26 +0200

    password checker

--- src/passwordchecker.js
+++ src/passwordchecker.js
@@ -1,24 +1,21 @@
-module.exports = {
-    passwordIsValid,
-    passwordIsOk
-}
+// module.exports = {
+//     passwordIsValid,
+//     passwordIsOk
+// }
 
 function passwordIsValid (password) {
-    var password = name;
-    try {
-        if (name == '') throw 'No password';
-        if (name == 'Sekgomotso#1') throw 'password exists';
-    }
-    catch(error) {
-        console.log(error);
-    }
 
     try {
-        if (passwordIsValid.length.value < 8) throw 'password denied';
-        if (passwordIsValid.length.value > 8) throw '';
+        if (password == '') throw 'No password';
+        if (password.length <= 8) throw 'password should be longer than 8 characters';
+        if (password.match(/[a-z]/) == null) throw 'password should have at least one lowercase letter.';
+        if (password.match(/[A-Z]/) == null) throw 'password should have at least one uppercase letter.';
+        if (password.match(/[0-9]/) == null) throw 'password should have at least one digit.';
+        if (password.match(/[{ # % & * " ']/) == null) throw 'password should have at least one special character.';
     }
     catch(error) {
         console.log(error);
     }
 }
 
+passwordIsValid('Sekgomotso#1');
