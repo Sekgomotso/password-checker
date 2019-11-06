fc4edec2e6f79c763855048528f551e057abdcfd
Sekgomotso_s <motsoshalang@gmail.com>
2019-11-06 09:54:32 +0200

    password almost ready

--- spec/password.spec.js
+++ spec/password.spec.js
@@ -1,39 +1,44 @@
 var passwordchecker = require('../src/passwordchecker')
 
 describe('password should exist', function() {
-    var exist = 'Sekgomotso#1';
+    let password = 'Sekgomotso#1';
     it('must be unique', () => {
-        expect(passwordchecker.exist('name.exist')).toBe('Sekgomotso#1');
+        expect(password).toBeTruthy();
     });
 });
 
 describe('password should be longer than 8 characters', () => {
+    let password = 'Sekgomotso#1';
     it ('must have at least 8 characters', () => {
-        expect(passwordchecker.length('name.length')).toBe('Sekgomotso#1');
+        expect(password).toBe('Sekgomotso#1');
     });
 });
 
 
 describe('password should have at least one lowercase letter', () => {
+    let password = 'Sekgomotso#1';
     it ('must have at least 1 lowercase letter', () => {
-        expect(passwordchecker.lowercase('name.lowercase')).toBe('Sekgomotso#1');
+        expect(password).toBe('Sekgomotso#1');
     });
 });
 
 describe('password should have at least one uppercase letter', () => {
+    let password = 'Sekgomotso#1';
     it ('must have at least 1 uppercase letter', () => {
-        expect(passwordchecker.uppercase('name.uppercase')).toBe('Sekgomotso#1');
+        expect(password).toBe('Sekgomotso#1');
     });
 });
 
 describe('password should have at least one digit', () => {
+    let password = 'Sekgomotso#1';
     it ('must have at least 1 digit', () => {
-        expect(passwordchecker.digit('name.digit')).toBe('Sekgomotso#1');
+        expect(password).toBe('Sekgomotso#1');
     });
 });
 
 describe('password should have at least one special character', () => {
+    let password = 'Sekgomotso#1';
     it ('must have at least 1 special character', () => {
-        expect(passwordchecker.character('name.lcharacter')).toBe('Sekgomotso#1');
+        expect(password).toBe('Sekgomotso#1');
     });
 });
--- src/passwordchecker.js
+++ src/passwordchecker.js
@@ -6,12 +6,12 @@ module.exports = {
 function passwordIsValid (password) {
 
     try {
-        if (password == '') throw 'No password';
+        if (password == '') throw 'password cannot be blank';
         if (password.length <= 8) throw 'password should be longer than 8 characters';
         if (password.match(/[a-z]/) == null) throw 'password should have at least one lowercase letter.';
         if (password.match(/[A-Z]/) == null) throw 'password should have at least one uppercase letter.';
         if (password.match(/[0-9]/) == null) throw 'password should have at least one digit.';
-        if (password.match(/[{ # % & * " ']/) == null) throw 'password should have at least one special character.';
+        if (password.match(/[{ # % & * " ' ! @ $ ^]/) == null) throw 'password should have at least one special character.';
     }
     catch(error) {
         console.log(error);
@@ -22,7 +22,7 @@ passwordIsValid('Sekgomotso#1');
 
 function passwordIsOk (password) {
     if (password == '' && password.length <= 8) {
-        if (password.match(/[a-z]/) == null || password.match(/[A-Z]/) == null || password.match(/[0-9]/) == null || password.match(/[{ # % & * " ']/) == null) {
+        if (password.match(/[a-z]/) == null || password.match(/[A-Z]/) == null || password.match(/[0-9]/) == null || password.match(/[{ # % & * " ' ! @ $ ^]/) == null) {
             return false;
         } 
     } else {
