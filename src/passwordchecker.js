6f86c3bd65e52af47589f4954339ef900ab9c51b
Sekgomotso_s <motsoshalang@gmail.com>
2019-11-11 17:09:46 +0200

    Password checker

--- src/passwordchecker.js
+++ src/passwordchecker.js
@@ -6,11 +6,22 @@ module.exports = {
 function passwordIsValid (password) {
 
     try {
-        if (password == '') throw 'password cannot be blank';
+        // password should exist
+        if (password == '') throw 'password cannot be blank'; 
+
+        // password should be longer than 8 characters
         if (password.length <= 8) throw 'password should be longer than 8 characters';
+
+        // password should have at least one lowercase letter
         if (password.match(/[a-z]/) == null) throw 'password should have at least one lowercase letter.';
+
+        // password should have at least one uppercase letter
         if (password.match(/[A-Z]/) == null) throw 'password should have at least one uppercase letter.';
+
+        // password should have at least one digit
         if (password.match(/[0-9]/) == null) throw 'password should have at least one digit.';
+
+        // password should have at least one special character
         if (password.match(/[{ # % & * " ' ! @ $ ^]/) == null) throw 'password should have at least one special character.';
     }
     catch(error) {
@@ -20,6 +31,7 @@ function passwordIsValid (password) {
 
 passwordIsValid('Sekgomotso#1');
 
+// If the given password meets at least three of the conditions listed above then this function should return true, otherwise it should return false.
 function passwordIsOk (password) {
     if (password == '' && password.length <= 8) {
         if (password.match(/[a-z]/) == null || password.match(/[A-Z]/) == null || password.match(/[0-9]/) == null || password.match(/[{ # % & * " ' ! @ $ ^]/) == null) {
